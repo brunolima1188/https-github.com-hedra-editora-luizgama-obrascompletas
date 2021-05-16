eb0af2e63e20248949a091512180f3d889cc6e4c
Jorge Sallum <jorge@hedra.com.br>
2021-05-16 18:33:21 -0300

    sed

---
+++ ORIGINAIS/sed.sh
@@ -0,0 +1,291 @@
+**LUIZ GAMA**
+**OBRAS COMPLETAS**
+**VOLUME IV**
+**DIREITO**
+**1870-1875**
+**HEDRA**
+**2021**
+**SUMÁRIO**
+**"EU ESTOU NO MEU POSTO DE HONRA" (1870)**
+**ACERTO DE CONTAS**
+**1.** *Calúnia Calculada*
+**2.** *Seriedade e riso*
+**3.** *Emancipação*
+**4.** *Firmino Moreira dos Santos*
+**PORTEIRAS DO VELHO OESTE**
+**5.** *O sr. Luiz Gama*
+**5. 1.** *Distinto redator*
+**6.** *Foro de Jundiaí --* *(Delegacia de Polícia)*
+**O JUIZ CRIMINOSO**
+**7.** *Para o sr. dr. juiz de direito ver*
+**8.** *Ainda a prisão do artista Leal, I*
+**9.** *Ainda a prisão do artista Leal, II*
+**10.** *Reforma do foro*
+**11.** *Tribunal do Júri*
+**PARADA REPUBLICANA**
+**12.** *A pensão aos filhos do senador Furtado*
+**O VELHO OESTE E O VELHO VALE MANDAM LEMBRANÇAS**
+**13.** *Jacareí*
+**14.** *Carta ao filho Benedicto Graccho Pinto da Gama*
+**15.** *Ao público*
+**16.** *Comarca de Campinas*
+**17.** *Carta a José Carlos Rodrigues*
+***SPARTACUS* DA GAMA**
+**18.** *Escândalo -- I*
+**18. 1.** *Escândalo -- I \[réplica\]*
+**19.** *Escândalo -- II*
+**20.** *Mais três\...*
+**O HOMEM QUE MAMOU O LEITE DO LIBERALISMO**
+**21.** *Coisas admiráveis*
+**21. 1.** *Cousas admiráveis \[réplica\]*
+**22.** *Cousas admiráveis*
+**22. 1.** *\[Tréplica de Raphael Tobias de Aguiar\]*
+**23.** *Cousas admiráveis*
+**24.** *Questão do pardo Narciso*
+**AFRO DÁ O PONTO FINAL**
+**25.** *Aos abolicionistas da escravidão*
+**"NOS OBLÍQUOS E SOMBRIOS BECOS DA CHICANA" (1871-1872)**
+**26.** *Carta a Ruy Barbosa*
+**27.** *Foro de Jundiaí --* *(Delegacia de Polícia), II*
+**28.** *Foro da Capital*
+**A MANHÃ DE 10 DE NOVEMBRO DE 1871**
+**29.** *Loja América*
+**30.** *Luiz G. P. da Gama*
+**31.** *Até que seja satisfeito*
+**32. *Província de S. Paulo*** *-- Foro da Capital*
+**33.** *Foro da Capital -- Juízo Municipal*
+**34.** *Jundiaí*
+**QUANDO O BECO DA CHICANA É SEM SAÍDA**
+**35.** *Caso Virgem*
+**35. 1.** *Caso Virgem* *\[réplica\]*
+**EMBOSCADA FORENSE**
+**36.** *Foro da Capital*
+**36. 1.** *Foro da Capital \[réplica\]*
+**37.** *Juízo de Direito*
+**38.** *Foro de Jacareí*
+**39.** *Foro de Jacareí*
+**40.** *Repartição da Polícia --* *Segurança Pública*
+**O JUIZ DO INFERNO**
+**41.** *Juízo Municipal -- Cousas do sapientíssimo sr. dr. Felicio, I*
+**42.** *Juízo Municipal -- Cousas do sapientíssimo sr. dr. Felicio, II*
+**43.** *Juízo Municipal -- Cousas do sapientíssimo sr. dr. Felicio,
+**"QUEM NÃO TEM PEITO NÃO TOMA MANDINGA!"**
+**44.** *O sr. Percy John Fryer*
+**44. 1.** *Juízo Municipal \[réplica\]*
+**45.** *Juízo Municipal -- Questão Fryer & Jones*
+**MISCELÂNEA: FILANTROPIA E REPÚBLICA**
+**46.** *Subscrição em favor da família do finado brigadeiro Oliveira*
+**47.** *Aos srs. assinantes da República*
+**OS POBRES DE SANTA IFIGÊNIA**
+**48.** *Carta ao sr. Eloy Ottoni*
+**48. 1.** *Carta a Luiz Gama*
+**49.** Ainda o congresso republicano em Itu
+**"NÃO SOU GRADUADO EM DIREITO" (1873-1875)**
+**INSTRUÇÕES ABOLICIONISTAS**
+**50.** *Carta a João Rodrigues de Oliveira China*
+**O IMPERADOR E A LIBERDADE: IMPRENSA E ALFORRIAS**
+**51.** *O imperador e a liberdade de imprensa, I*
+**52.** *O imperador e a liberdade de imprensa, II*
+**53.** *Questão manumissória --Petição dirigida ao governo imperial*
+**LUIZ GONZAGA *AFRO* DA GAMA**
+**54.** *\[Sobre a comissão de classificação de escravos\]*
+**55.** *Franca ao imperador*
+**56.** *Aos srs. redatores de jornais*
+**ARGÚCIAS DA CHICANA**
+**57.** *O julgamento da falência de Julio Geraud*
+**57. 1.** *A propósito do julgamento da falência de Julio Geraud
+**58.** *Egrégio Tribunal da Relação -- J. Geraud -- Petição de
+**MISCELÂNEA: UM INCÊNDIO E TRÊS PEDIDOS**
+**59.** *Ao público*
+**60.** *Carta a Salvador de Mendonça*
+**61.** *Ao sr. exmo. dr. ministro da Justiça*
+**62.** *Ribeirão Preto*
+**EU ESTOU NO MEU POSTO DE HONRA (1870)**
+**\*didascália\***
+**ACERTO DE CONTAS**
+**\*didascália\***
+**1. CALÚNIA CALCULADA**[^1]
+**\*didascália\***
+**2. SERIEDADE E RISO**[^9]
+**\*didascália\***
+**3. EMANCIPAÇÃO**[^27]
+**\*didascália\***
+**4. FIRMINO MOREIRA DOS SANTOS**[^33]
+**\*didascália\***
+**PORTEIRAS DO VELHO OESTE**
+**\*didascália\***
+**5. O SR. LUIZ GAMA**[^41]
+**\*didascália\***
+**5. 1. DISTINTO REDATOR \[réplica\]**[^46]
+**\*didascália\***
+**6. FORO DE JUNDIAÍ** -- **(DELEGACIA DE POLÍCIA)**[^52]
+**\*didascália\***
+**JUIZ CRIMINOSO**
+**\*didascália\***
+**7. PARA O SR. DR. JUIZ DE DIREITO VER**[^76]
+**\*didascália\***
+**8. AINDA A PRISÃO DO ARTISTA LEAL \[I\]**[^89]
+**\*didascália\***
+**9. AINDA A PRISÃO DO ARTISTA LEAL \[II\]**[^115]
+**\*didascália\***
+**10. REFORMA DO FORO**[^147]
+**\*didascália\***
+**11. TRIBUNAL DO JÚRI**[^165]
+**\*didascália\***
+**PARADA REPUBLICANA**
+**\*didascália\***
+**12. A PENSÃO AOS FILHOS DO SENADOR FURTADO**[^173]
+**\*didascália\***
+**O VELHO OESTE -- E O VELHO VALE! -- MANDA LEMBRANÇAS**
+**\*didascália\***
+**13. JACAREÍ**[^178]
+**\*didascália\***
+**14. CARTA AO FILHO BENEDICTO GRACCHO PINTO DA GAMA**[^195]
+**\*didascália\***
+**15. AO PÚBLICO**[^200]
+**\*didascália\***
+**16. COMARCA DE CAMPINAS**[^203]
+**\*didascália\***
+**17. CARTA A JOSÉ CARLOS RODRIGUES**[^223]
+**\*didascália\***
+***SPARTACUS* DA GAMA**
+**18. ESCÂNDALO -- I**[^240]
+**\*****didascália\***
+**18. 1. ESCÂNDALO -- I \[réplica\]**[^241]
+**\*didascália\***
+**19. ESCÂNDALO -- II**[^244]
+**\*didascália\***
+**20. MAIS TRÊS\...**[^247]
+**\*didascália\***
+**O HOMEM QUE MAMOU O LEITE DO LIBERALISMO**
+**\*didascália\***
+**21. COISAS ADMIRÁVEIS**^⁠^[^252]
+**\*didascália\***
+**21. 1. COUSAS ADMIRÁVEIS \[réplica\]**[^264]
+**\*didascália\***
+**22. COUSAS ADMIRÁVEIS**[^265]
+**\*didascália\***
+**22. 1. \[TRÉPLICA DE RAPHAEL TOBIAS DE AGUIAR\]**[^277]
+**\*didascália\***
+**23. COUSAS ADMIRÁVEIS**[^283]
+**\*didascália\***
+**24. QUESTÃO DO PARDO NARCISO**[^299]
+**\*didascália\***
+**AFRO DÁ O PONTO FINAL**
+**\*didascália\***
+**25. AOS ABOLICIONISTAS DA ESCRAVIDÃO**[^317]
+**\*didascália\***
+**NOS OBLÍQUOS E SOMBRIOS BECOS DA CHICANA (1871-1872)**
+**\*didascália\***
+**26. CARTA A RUY BARBOSA**[^323]
+**\*didascália\***
+**27. FORO DE JUNDIAÍ -- (Delegacia de Polícia)**[^329]
+**\*didascália\***
+**28. FORO DA CAPITAL**[^354]
+**\*didascália\***
+**A MANHÃ DE 10 DE NOVEMBRO DE 1871**
+**\*didascália\***
+**29. LOJA AMÉRICA**[^360]
+**\*didascália\***
+**30. LUIZ G. P. GAMA**[^386]
+**\*didascália\***
+**31. ATÉ QUE SEJA SATISFEITO**[^401]
+**\*didascália\***
+**32. PROVÍNCIA DE S. PAULO -- FORO DA CAPITAL**[^405]
+**\*didascália\***
+**33. FORO DA CAPITAL -- JUÍZO MUNICIPAL**[^408]
+**\*didascália\***
+**34. JUNDIAÍ**[^416]
+**\*didascália\***
+**QUANDO O BECO DA CHICANA É SEM SAÍDA**
+**\*didascália\***
+**35. CASO VIRGEM**[^425]
+**\*didascália\***
+**35. 1. CASO VIRGEM \[réplica\]**[^431]
+**\*didascália\***
+**EMBOSCADA FORENSE**
+**\*didascália\***
+**36. FORO DA CAPITAL**[^439]
+**\*didascália\***
+**36. 1. FORO DA CAPITAL \[réplica\]**[^459]
+**\*didascália\***
+**37. JUÍZO DE DIREITO**[^462]
+**\*didascália\***
+**MÍSEROS LIBERTOS SEPULTADOS VIVOS EM BÁRBARA ESCRAVIDÃO**
+**38. FORO DE JACAREÍ**[^467]
+**\*didascália\***
+**39. FORO DE JACAREÍ**[^475]**\
+**
+**\*didascália\***
+**40. REPARTIÇÃO DA POLÍCIA**[^498]
+**\*didascália\***
+**O JUIZ DO INFERNO**
+**\*didascália\***
+**41. *COUSAS DO SAPIENTÍSSIMO SR. DR. FELICIO***[^515]
+**\*didascália\***
+**42. *COUSAS DO SAPIENTÍSSIMO SR. DR. FELICIO***[^537]
+**\*didascália\***
+**43. *COUSAS DO SAPIENTÍSSIMO SR. DR. FELICIO***[^553]
+**\*didascália\***
+**QUEM NÃO TEM PEITO NÃO TOMA MANDINGA!**
+**\*didascália\***
+**44. O SR. PERCY JOHN FRYER**[^591]
+**\*didascália\***
+**44. 1. JUÍZO MUNICIPAL \[réplica\]**[^598]
+**\*didascália\***
+**45. QUESTÃO FRYER & JONES -- *CARTA AO ADVOGADO DR. PEREIRA PINTO
+**\*didascália\***
+**MISCELÂNEA: FILANTROPIA E REPÚBLICA**
+**\*didascália\***
+**46. SUBSCRIÇÃO EM FAVOR DA FAMÍLIA DO FINADO BRIGADEIRO
+**\*didascália\***
+**47. AOS SRS. ASSINANTES DA REPÚBLICA**[^672]
+**\*didascália\***
+**OS POBRES DE SANTA IFIGÊNIA**
+**\*didascália\***
+**48. CARTA AO SR. ELOY OTTONI**[^675]
+**\*didascália\***
+**48. 1. CARTA A LUIZ GAMA**[^681]
+**\*didascália\***
+**49. AINDA O CONGRESSO REPUBLICANO EM ITU**[^685]
+**\*didascália\***
+**NÃO SOU GRADUADO EM DIREITO (1873-1875)**
+**didascália\***
+**INSTRUÇÕES ABOLICIONISTAS**
+**didascália\***
+**50. CARTA A JOÃO RODRIGUES DE OLIVEIRA CHINA**
+**\*didascália\***
+**O IMPERADOR E A LIBERDADE: IMPRENSA E ALFORRIAS**
+**\*didascália\***
+**51. O IMPERADOR E A LIBERDADE DE IMPRENSA**[^695]
+**\*didascália\***
+**52. O IMPERADOR E A LIBERDADE DE IMPRENSA**[^698]
+**\*didascália\***
+**53. QUESTÃO MANUMISSÓRIA** -- **PETIÇÃO DIRIGIDA AO GOVERNO
+**\*didascália\***
+**LUIZ GONZAGA *AFRO* DA GAMA**
+**\*didascália\***
+**54. \[SOBRE A COMISSÃO DE CLASSIFICAÇÃO DE ESCRAVOS\]**[^714]
+**\*didascália\***
+**55. FRANCA AO IMPERADOR**[^718]
+**\*didascália\***
+**56. AOS SRS. REDATORES DE JORNAIS**[^721]
+**\*didascália\***
+**ARGÚCIAS DA CHICANA**
+**\*didascália\***
+**57. O JULGAMENTO DA FALÊNCIA DE JULIO GERAUD**[^731]
+**\*didascália\***
+**57. 1. A PROPÓSITO DO JULGAMENTO DA FALÊNCIA DE JULIO GERAUD**[^737]
+**\*didascália\***
+**58. EGRÉGIO TRIBUNAL DA RELAÇÃO *--* J. GERAUD -- PETIÇÃO DE
+**\*didascália\***
+**MISCELÂNEA: UM INCÊNDIO E TRÊS PEDIDOS**
+**\*didascália\***
+**59. AO PÚBLICO**[^764]
+**\*didascália\***
+**60. CARTA A SALVADOR DE MENDONÇA**[^769]
+**\*didascália\***
+**61. AO SR. EXMO. SR. MINISTRO DA JUSTIÇA**[^771]
+**\*didascália\***
+**62. RIBEIRÃO PRETO**[^775]
+**\*didascália\***
